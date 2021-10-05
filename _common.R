ebad6569d0f1c8a330153da8eba0d6a6fdc36bd7
ddauber <62331069+ddauber@users.noreply.github.com>
2021-10-05 18:32:22 +0100

    Added back in `_common.R`

---
+++ _common.R
@@ -0,0 +1,26 @@
+# Commonly run R code BEFORE all chapters
+
+# Format code chunks
+knitr::opts_chunk$set(
+  comment = "#>",
+  class.source = "customCodeChunk",
+  collapse = TRUE,
+  fig.align = "center"
+)
+
+# Truncate output, specified as 'out.lines = n'
+# save the built-in output hook
+hook_output <- knitr::knit_hooks$get("output")
+
+# set a new output hook to truncate text output
+knitr::knit_hooks$set(output = function(x, options) {
+  if (!is.null(n <- options$out.lines)) {
+    x <- xfun::split_lines(x)
+    if (length(x) > n) {
+      # truncate the output
+      x <- c(head(x, n), "....\n")
+    }
+    x <- paste(x, collapse = "\n")
+  }
+  hook_output(x, options)
+})
